4884fe22e934d030be9a7591f4809f2867e8b569
Florent-Lefevre <lefevre.flo@gmail.com>
2022-03-07 08:58:34 +0100

    files created

--- portfolio-front/src/components/profil/Profil.jsx
+++ portfolio-front/src/components/profil/Profil.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import profil from '../../assets/pictures/profil.jpeg'
 import './Profil.scss'
 
 const Profil = () => {
   return (
     <div className='Profil'>
         <h1>Profilgfsdgfsdgfdgdfsgdrezrezf</h1>
+        <img src={profil} alt="profil picture" className='profil-picture'/>
     </div>
   )
 }
